b5a4e3e24be84dafafd7ad2ddee976e4aa38a569
ical10 <mochhusnir@gmail.com>
2022-11-21 22:47:28 +0700

    feat: create getAllPublicSpaces and getAllPostsBySpaceId

--- src/hooks/use-subsocial-api.ts
+++ src/hooks/use-subsocial-api.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { bnsToIds } from "@subsocial/utils";
 import type { SpaceData, PostData } from "@subsocial/api/types";
 import initializeApi from "src/lib/SubsocialApi";
@@ -12,10 +12,33 @@ export const useSubSocialApiHook = () => {
   const [loadingSpaces, setLoadingSpaces] = useState(false);
   const [loadingCreatePost, setLoadingCreatePost] = useState(false);
 
+  useEffect(() => {
+    if (subsocialApi) {
+      getAllPublicSpaces();
+    }
+  }, [subsocialApi]);
+
   const initApi = async (): Promise<void> => {
     try {
       const api = await initializeApi();
       setSubsocialApi(api);
+    } catch (error) {
+      console.warn({ error });
+    }
+  };
+
+  const getAllPublicSpaces = async (): Promise<void> => {
+    try {
+      const myAddress = "5DSg6JpKCjKVSEEKzVtoSkszpMu3NUfWEs7WiDcCxzhXksCV";
+
+      // Fetching ids of all the spaces by owner.
+      const spaceIds = await subsocialApi?.blockchain.spaceIdsByOwner(
+        myAddress
+      );
+
+      //TODO: handle undefined error
+      const spaces = await subsocialApi?.findPublicSpaces(bnsToIds(spaceIds!));
+      setSpaces(spaces!);
     } catch (error) {
       console.warn({ error });
     }
@@ -51,11 +74,29 @@ export const useSubSocialApiHook = () => {
     }
   };
 
+  const getAllPostsBySpaceId = async (spaceId: string): Promise<void> => {
+    setLoading(true);
+
+    try {
+      const postIds = await subsocialApi?.blockchain.postIdsBySpaceId(spaceId);
+      const posts = await subsocialApi?.findPublicPosts(bnsToIds(postIds!));
+      if (posts) {
+        setPosts(posts);
+      }
+    } catch (error) {
+      console.warn({ error });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     subsocialApi,
     loading,
     initApi,
+    spaces,
     getAllPosts,
     posts,
+    getAllPostsBySpaceId,
   };
 };
