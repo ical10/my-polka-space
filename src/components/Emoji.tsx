a986372d1e9eb25f8dbfbcce987edfb812a642e0
ical10 <mochhusnir@gmail.com>
2022-11-22 15:05:14 +0700

    feat: create Emoji component

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,15 @@
+type EmojiProps = {
+  symbol: string;
+  label?: string;
+};
+const Emoji = ({ label, symbol }: EmojiProps) => (
+  <span
+    className="emoji"
+    role="img"
+    aria-label={label ? label : ""}
+    aria-hidden={label ? "false" : "true"}
+  >
+    {symbol}
+  </span>
+);
+export default Emoji;
