89aa5d9bf996de874a635a851a1f80589a3de709
ical10 <mochhusnir@gmail.com>
2022-11-22 14:46:07 +0700

    feat: override light with custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#fc999b",
+          secondary: "#e88f9d",
+          accent: "#fcc392",
+          neutral: "#211F2E",
+          "base-100": "#EEEFF1",
+          info: "#9BBDF8",
+          success: "#5BECC7",
+          warning: "#F3AB12",
+          error: "#F57072",
+        },
+      },
+      "dark",
+    ],
   },
 };
