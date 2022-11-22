5952802059ae2f74ff4039ba67707810fd57f85d
ical10 <mochhusnir@gmail.com>
2022-11-23 00:06:22 +0700

    feat: create string helper fx to trim address

---
+++ src/helpers/strings.ts
@@ -0,0 +1,10 @@
+const trimMiddleString = (text?: string, numberStringsKept = 5) => {
+  if (!text) return "";
+  const temp = `${text.slice(0, numberStringsKept)}...${text.slice(
+    text.length - numberStringsKept
+  )}`;
+
+  return temp;
+};
+
+export { trimMiddleString };
