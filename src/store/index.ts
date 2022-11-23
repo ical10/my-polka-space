03567c733700dbf7b02b27eb1234adac14562ab1
ical10 <mochhusnir@gmail.com>
2022-11-23 21:06:10 +0700

    chore: add new states

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,9 +4,13 @@ import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
 interface WalletState {
   account: InjectedAccountWithMeta | null;
   setAccount: (newAccount: InjectedAccountWithMeta) => void;
+  accounts: InjectedAccountWithMeta[] | null;
+  setAccounts: (newAccounts: InjectedAccountWithMeta[]) => void;
 }
 
 export const useWalletStore = create<WalletState>()((set) => ({
   account: null,
   setAccount: (newAccount) => set(() => ({ account: newAccount })),
+  accounts: null,
+  setAccounts: (newAccounts) => set(() => ({ accounts: [...newAccounts] })),
 }));
