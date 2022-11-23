b6f50a1ac8fbf0a81f6d2f92accce0fa9efeaedd
ical10 <mochhusnir@gmail.com>
2022-11-23 23:16:58 +0700

    feat: add emoji to SelectTheme

--- src/components/SelectTheme.tsx
+++ src/components/SelectTheme.tsx
@@ -1,14 +1,17 @@
+import Emoji from "src/components/Emoji";
+
 const SelectTheme = () => {
   return (
-    <select
-      className="select select-secondary w-full max-w-xs"
-      data-choose-theme
-    >
+    <select className="select select-secondary" data-choose-theme>
       <option disabled selected>
         Pick your theme
       </option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
+      <option value="dark">
+        <Emoji symbol="🌑" /> Dark
+      </option>
+      <option value="light">
+        <Emoji symbol="☀️" /> Light
+      </option>
     </select>
   );
 };
