97f0556427a57dbee4d525588f21b5daac1728f2
ical10 <mochhusnir@gmail.com>
2022-11-23 23:17:36 +0700

    feat: create cards to be selected

--- pages/customize.tsx
+++ pages/customize.tsx
@@ -19,8 +19,33 @@ const Customize = () => {
 
       <Layout>
         <div className="flex flex-col gap-8">
-          <div className="text-xl font-semibold">
-            Customize your Post card here
+          <div className="border-2 border-black">
+            <div className="card rounded-none card-side bg-base-100 shadow-xl">
+              <figure>
+                <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">New movie is released!</h2>
+                <p>Click the button to watch on Jetflix app.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Watch</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="border-2 border-red-500">
+            <div className="card rounded-2xl card-side bg-base-100 shadow-xl">
+              <figure>
+                <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">New movie is released!</h2>
+                <p>Click the button to watch on Jetflix app.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Watch</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
