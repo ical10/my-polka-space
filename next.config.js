d3463ebf83c7e94121958e0c678966a7719992be
ical10 <mochhusnir@gmail.com>
2022-11-23 15:52:12 +0700

    chore: remove mnemonic

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   swcMinify: true,
   publicRuntimeConfig: {
     apiAuthHeader: process.env.NEXT_PUBLIC_AUTH_HEADER,
-    mnemonic: process.env.NEXT_PUBLIC_MNEMONIC,
   },
 };
 
